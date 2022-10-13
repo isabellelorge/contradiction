d577a7f4f52b09ee361f8477e37d839c3e6641d8
Isabelle Lorge <isabelle.lorge@outlook.com>
2022-10-13 10:32:07 +0100

    files

---
+++ src/helper_functions.py
@@ -0,0 +1,11 @@
+# function to extract entities (< Spacy)
+
+# function to extract common concepts (stem + one-hop Wordnet?)
+
+# function to get prediction from fine-tuned BERT model for stance (put code to fine-tune model..? takes only a few min)
+
+# function to get cosine sim prediction for stance FOR/AGAINST from SBERT
+
+# function to filter/combine entities from full dict (eg, Dems/democrat, Trump/Donald Trump, US/USA/America, etc.)
+
+# function resolve coreferences?
